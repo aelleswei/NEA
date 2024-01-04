c9cfec6e181b6ef8cc254e297ac6920ed0887b9b
aelleswei <112623582+aelleswei@users.noreply.github.com>
2024-01-04 15:07:59 +0000

    Add files via upload

--- MainGame.py
+++ MainGame.py
@@ -3,6 +3,7 @@ from player_prototype import Player
 from settings2 import *
 from level2 import plain
 from block_new import Block
+from math import sqrt
 
 class MainGame():
 	def __init__(self):
@@ -20,7 +21,7 @@ class MainGame():
 	def run(self):
 		clock.tick(FPS)
 		win.blit(self.bg,(0,0))
- 
+		self.bg = pygame.image.load('graphics/level-creator-bg.png')
 		mouse = pygame.mouse.get_pos()
 		tile_x = mouse[0] // 64
 		tile_y = mouse[1] // 64
@@ -28,14 +29,15 @@ class MainGame():
 		current_tiles = (tile_x,tile_y)
 		#print(current_tiles)
 		keys = pygame.key.get_pressed()
-		if keys[pygame.K_1]:
-			self.block_selected = "block"
+
+		def change_mouse(type):
 			pygame.mouse.set_visible(False)
-			block_img = pygame.image.load('graphics/block.png')
-			block_img = pygame.transform.rotozoom(block_img,0,0.8)
-			block_rect = block_img.get_rect()
-			block_rect.center = mouse
-			win.blit(block_img,block_rect.center)
+			mouse_img = pygame.image.load(f'graphics/{type}.png')
+			mouse_img = pygame.transform.rotozoom(mouse_img,0,0.8)
+			mouse_rect = mouse_img.get_rect()
+			mouse_rect.center = mouse
+			win.blit(mouse_img,mouse_rect.center)
+
 
 
 		def default_draw():
@@ -60,7 +62,8 @@ class MainGame():
 
 			return valid
 
-		def player_valid(current):
+
+		def check_around(current):
 			current = current
 			array = []
 			tile0 = check_tile(current,-1,0)
@@ -94,6 +97,14 @@ class MainGame():
 			tile7 = check_tile(current,1,-1)
 			if valid_tile(tile7) == True:
 				array.append(tile7)
+
+			return array
+
+
+		def player_valid(current):
+			current = current
+			
+			array = check_around(current)
 			
 			#print(array)
 			
@@ -112,25 +123,79 @@ class MainGame():
 
 			return valid
 
+		def block_valid(current):
+			current = current
+			array = check_around(current)
 
-		if keys[pygame.K_1]:
-			self.block_selected = "block"
-			pygame.mouse.set_visible(False)
-			block_img = pygame.image.load('graphics/block.png')
-			block_img = pygame.transform.rotozoom(block_img,0,0.8)
-			block_rect = block_img.get_rect()
-			block_rect.center = mouse
-			win.blit(block_img,block_rect.center)
+			for tile in array:
+				if plain[tile[1]][tile[0]] == 'p':
+					valid = False
+					break
+				elif plain[current[1]][current[0]] == 'p':
+					valid = False
+					break
+
+				else:
+					valid = True
+
+			return valid
+
+		def enemy_valid(current):
+			current = current
+			for i in range(len(plain)):
+				for j in range(len(plain[i])):
+					if plain[i][j] == 'p':
+						player_tile = (j,i)
+						print(tile)
+						d = sqrt((current[0] - j)**2 + (current[1] - i)**2)
+						print(d)
+						if d > 3:
+							valid = True
+							break
+						else:
+							valid = False
+							tile_invalid = (j,i)
+
+
+
+
+					
+
+			
+
+
+
+
+
 			for i in range(len(plain)):
 					tile_y = i
 					for j in range(len(plain[i])):
 						tile_x = j
-						if plain[i][j] != "":
+						if tile_invalid == (j,i):
+							invalid_block = Block(j,i,'graphics/invalid_block.png')
+							invalid_block.draw()
+
+			
+
+
+
+		def screen_check(type):
+			for i in range(len(plain)):
+					tile_y = i
+					for j in range(len(plain[i])):
+						tile_x = j
+						valid = type((j,i))
+						if valid == False:
 							invalid_block = Block(j,i,'graphics/invalid_block.png')
 							invalid_block.draw()
 
 
-			if pygame.mouse.get_pressed()[0] and plain[current_tiles[1]][current_tiles[0]] == "":
+		if keys[pygame.K_1]:
+			self.block_selected = "block"
+			change_mouse(self.block_selected)
+			screen_check(block_valid)
+
+			if pygame.mouse.get_pressed()[0] and block_valid(current_tiles):
 				plain[current_tiles[1]][current_tiles[0]] = "B"
 				block_block = Block(current_tiles[0],current_tiles[1],'graphics/block.png')
 				self.block_group.add(block_block)
@@ -138,35 +203,27 @@ class MainGame():
 
 		if keys[pygame.K_2]:
 			self.block_selected = 'player'
-			pygame.mouse.set_visible(False)
-			block_img = pygame.image.load('graphics/player.png')
-			block_img = pygame.transform.rotozoom(block_img,0,0.8)
-			block_rect = block_img.get_rect()
-			block_rect.center = mouse
-			win.blit(block_img,block_rect.center)
-			for i in range(len(plain)):
-				tile_y = i
-				for j in range(len(plain[i])):
-					tile_x = j
-					valid = player_valid((j,i))
-					if valid == False:
-						invalid_block = Block(j,i,'graphics/invalid_block.png')
-						invalid_block.draw()
+			change_mouse(self.block_selected)
+			screen_check(player_valid)
 
-			if pygame.mouse.get_pressed()[0] and player_valid(current_tiles):
+			if pygame.mouse.get_pressed()[0] and player_valid(current_tiles) and self.player_count == 0:
 				plain[current_tiles[1]][current_tiles[0]] = "P"
 				player_block = Block(current_tiles[0],current_tiles[1],'graphics/player.png')
 				self.block_group.add(player_block)
 				self.player_count += 1
+				for tile in check_around(current_tiles):
+					plain[current_tiles[1]][current_tiles[0]] = 'p'
+
 
 			if self.player_count > 0:
-				for i in range(len(plain)):
-					tile_y = i
-					for j in range(len(plain[i])):
-						tile_x = j
-						invalid_block = Block(j,i,'graphics/invalid_block.png')
-						invalid_block.draw()
+				self.bg = pygame.image.load('graphics/level-creator-bg1.png')
 
+
+
+		if keys[pygame.K_3]:
+			self.block_selected = 'enemy1'
+			change_mouse(self.block_selected)
+			enemy_valid(current_tiles)
 
 
 
